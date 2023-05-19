c8a000aebdb82868e9b6c4e4301d9760d492c084
zaynOm <ouaziz0626@gmail.com>
2023-05-19 21:19:04 +0100

    adding cd built-in cmd

--- shell.h
+++ shell.h
@@ -10,17 +10,22 @@
 
 extern char **environ;
 
-int isnumber(char *s);
-char *_strcat(char *dest, char *src);
 void exec_cmd(char **arg, char *av);
 char **getcmd();
-
 char **split_string(char *s, int n, char delimiter);
 
-int _atoi(char *s);
+/* string manipulation */
 char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 int _strlen(char *s);
+
+/* number manipulation */
+int _atoi(char *s);
 int _isdigit(int c);
+int isnumber(char *s);
+
+/* built-in commands */
+void cd(char *arg, char *av);
 
 #endif /* SHELL_H */
