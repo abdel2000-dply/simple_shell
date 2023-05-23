70a65c7c313538a72dacbfc1726575aee5607d3c
zaynOm <ouaziz0626@gmail.com>
2023-05-23 19:27:47 +0100

    adding non-interactive & script modes

--- shell.c
+++ shell.c
@@ -1,22 +1,24 @@
 #include "shell.h"
 
-int main(int __attribute__((unused)) ac, char **av)
+void intmode(char **av)
 {
 	char **arg, **cmds;
-	int exit_code, i, j;
+	int exit_code, i;
 
-	while (isatty(STDIN_FILENO))
+	while (1)
 	{
-		printf("$ ");
+		if (isatty(STDIN_FILENO))
+			printf("$ ");
+		fflush(stdout);
 
-		cmds = getcmd();
+		cmds = getcmd(STDIN_FILENO);
+
+		if (!cmds[0])
+			continue;
+
 		for (i = 0; cmds[i]; i++)
 		{
-			j = _strlen(cmds[i]);
-
-			arg = split_string(cmds[i], j, ' ');
-			if (!arg[0])
-				continue;
+			arg = split_string(cmds[i], _strlen(cmds[i]), ' ');
 
 			exit_code = (isnumber(arg[1])) ? _atoi(arg[1]) : 100;
 
@@ -29,6 +31,33 @@ int main(int __attribute__((unused)) ac, char **av)
 		}
 		free(cmds);
 	}
+}
+
+void filemode(char **av)
+{
+	int fd;
+	char **cmds, **arg;
+
+
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		perror(av[1]);
+
+	while ((cmds = getcmd(fd)) != NULL)
+	{
+		if (!cmds[0])
+		continue;
+		arg = split_string(cmds[0], _strlen(cmds[0]), ' ');
+		exec_cmd(arg, av[0]);
+	}
+}
+
+int main(int __attribute__((unused)) ac, char **av)
+{
+	if (ac > 1)
+		filemode(av);
+	else
+		intmode(av);
 
 	return (0);
 }
