7012bbc16e616656a90f9aaea8f02b7d5d8c1845
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2023-05-24 23:13:33 +0000

    updating getcmd from returning a double pointer to a pointer

--- getcmd.c
+++ getcmd.c
@@ -1,15 +1,12 @@
 #include "shell.h"
 
-char **getcmd(int fd)
+char *getcmd(int fd)
 {
 	char **arg, *line = NULL;
-	
+
 	line = _getline(fd);
 	if (line == NULL)
 		exit(0);
 
-	arg = split_string(line, _strlen(line), ';');
-
-	free(line);
-	return (arg);
+	return (line);
 }
