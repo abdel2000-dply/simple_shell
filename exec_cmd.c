40f8b1c8ffd1973af548b43b47d18d3b2fb672de
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2023-05-24 23:33:15 +0000

    merging the new changes to master branch 🎇

--- exec_cmd.c
+++ exec_cmd.c
@@ -9,6 +9,7 @@ typedef struct builtin
 void exec_cmd(char **arg, char *av)
 {
 	char *bin_arg;
+<<<<<<< HEAD
 	int i = 0;
 	builtin b[] = {
 		{"cd", cd},
@@ -19,6 +20,18 @@ void exec_cmd(char **arg, char *av)
 		{NULL, NULL},
 	};
 	while (b[i].cmd)
+=======
+	pid_t pid;
+
+	if (!strcmp("cd", arg[0]))
+	{
+		cd(arg[1], av);
+		return;
+	}
+
+	bin_arg = _strcat("/usr/bin/", arg[0]);
+	if (!access(arg[0], F_OK) && !access(bin_arg, F_OK))
+>>>>>>> c8a000a (adding cd built-in cmd)
 	{
 		if (!strcmp(b[i].cmd, arg[0]))
 		{
@@ -36,7 +49,12 @@ void exec_cmd(char **arg, char *av)
 		return;
 	}
 
+<<<<<<< HEAD
 	if (fork() == 0)
+=======
+	pid = fork();
+	if (pid == 0)
+>>>>>>> c8a000a (adding cd built-in cmd)
 	{
 		if (execve(arg[0], arg, NULL) == -1)
 			execve(bin_arg, arg, environ);
