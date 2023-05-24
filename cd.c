718092cd1cc944afde32a50d5fc9f392bc083432
zaynOm <ouaziz0626@gmail.com>
2023-05-24 22:56:46 +0100

    update PWD

--- cd.c
+++ cd.c
@@ -2,39 +2,52 @@
 
 char last_dir[256] = {0};
 
-void cd(char *arg, char *av)
+int cd(char **arg, char *av)
 {
 	char *home, curr_dir[256];
+	char *pwd[] = {"", "PWD", ""};
 
-	if (!arg)
+	if (!arg[1])
 	{
 		getcwd(last_dir, sizeof(last_dir));
-		home = getenv("HOME");
-		chdir(home);
+		home = _getenv("HOME");
+		if (chdir(home))
+		{
+			perror("cd");
+			return (-1);
+		}
+		pwd[2] = home;
+		_setenv(pwd, av);
+		
 	}
-	else if (!_strcmp("-", arg)) 
+	else if (!_strcmp("-", arg[1])) 
 	{
 		if (!last_dir[0])
 		{
 			printf("%s: cd: OLDPWD not set\n", av);
-			return;
+			return (-1);
 		}
 
 		getcwd(curr_dir, sizeof(curr_dir));
 		chdir(last_dir);
 		printf("%s\n", last_dir);
 		_strcpy(last_dir, curr_dir);
+		getcwd(pwd[2], sizeof(pwd[2]));
+		_setenv(pwd, av);
 	}
 	else
 	{
 		getcwd(curr_dir, sizeof(curr_dir));
-		if (chdir(arg) == -1)
+		if (chdir(arg[1]) == -1)
 		{
-			printf("%s: cd: ", av);
+			dprintf(STDERR_FILENO, "%s: cd: ", av);
 			fflush(stdout);
-			perror(arg);
+			perror(arg[1]);
+			return (-1);
 		}
-		else
-			_strcpy(last_dir, curr_dir);
+		pwd[2] = arg[1];
+		_strcpy(last_dir, curr_dir);
+		_setenv(pwd, av);
 	}
+	return (0);
 }
