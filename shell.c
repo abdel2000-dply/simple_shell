e13b535ee78dedbb7d7ea9bc6484c46b2ae9d0d8
zaynOm <ouaziz0626@gmail.com>
2023-05-26 02:08:55 +0100

    fixing issue

--- shell.c
+++ shell.c
@@ -8,7 +8,6 @@ void intmode(char **av)
 {
 	char **arg, **cmds, *line;
 	int len, i;
-	char *del;
 
 	while (1)
 	{
@@ -18,33 +17,22 @@ void intmode(char **av)
 
 		line = getcmd(STDIN_FILENO);
 		len = _strlen(line);
-		del = delimiter_collector(line, len);
 
-		if (del != NULL)
-		{
-			cmds = split_string(line, len, del[0]);
-			cmds = spliting_bydelimiter(cmds, del);
-		}
-		else
-		{
-			cmds = malloc(2 * sizeof(char *));
-			cmds[0] = line;
-			cmds[1] = NULL;
-		}
-
-		if (!cmds[0])
-			continue;
+		cmds = split_string(line, len, ';');
 
 		for (i = 0; cmds[i]; i++)
 		{
 			arg = split_string(cmds[i], _strlen(cmds[i]), ' ');
 
+			if (!arg[0])
+				continue;
+
 			exec_cmd(arg, av[0]);
 
-			free(arg);
+			free_array(arg);
 		}
-		free(del);
-		free(cmds);
+		free(line);
+		free_array(cmds);
 	}
 }
 
@@ -54,9 +42,8 @@ void intmode(char **av)
  */
 void filemode(char **av)
 {
-	int fd;
 	char **cmds, *line;
-
+	int fd;
 
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
@@ -64,12 +51,16 @@ void filemode(char **av)
 
 	while ((line = getcmd(fd)) != NULL)
 	{
-		if (!line)
+		cmds = split_string(line, _strlen(line), ' ');
+		if (!cmds[0])
 			continue;
 
-		cmds = split_string(line, _strlen(line), ' ');
 		exec_cmd(cmds, av[0]);
+
+		free(line);
+		free_array(cmds);
 	}
+	close(fd);
 }
 
 /**
