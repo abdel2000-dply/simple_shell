573723ef3496015c335885ac22720cdaf5f1378d
zaynom <ouaziz0626@gmail.com>
2023-05-26 13:50:18 -0700

    passing r_size instead of size of pointer * r_size

--- split_str.c
+++ split_str.c
@@ -9,7 +9,7 @@
 
 char **split_string(char *s, int len, char delim)
 {
-	char *word, **result = NULL;
+	char *word, **result = {NULL};
 	int r_size, w_count, in_word, w_len, start, end, i;
 
 	r_size = w_count = in_word = 0;
@@ -26,7 +26,7 @@ char **split_string(char *s, int len, char delim)
 				word = malloc(sizeof(char) * (w_len + 1));
 				_memcpy(word, s + start, w_len);
 				word[w_len] = '\0';
-				result = _realloc_array(result, sizeof(char *) * (++r_size));
+				result = _realloc_array(result, ++r_size);
 				result[w_count] = word;
 				w_count++, in_word = 0;
 			}
@@ -40,7 +40,7 @@ char **split_string(char *s, int len, char delim)
 			break;
 	}
 
-	result = _realloc_array(result, sizeof(char *) * (++r_size));
+	result = _realloc_array(result, ++r_size);
 	result[w_count] = NULL;
 
 	return (result);
