69f4a234c162c16366411242b5811d3162f21759
abdel2000-dply <abno870@gmail.com>
2023-05-27 03:15:49 +0100

    exit testing

--- exit.c
+++ exit.c
@@ -12,10 +12,16 @@ int ex(char **arg, char *av)
 	int exit_code;
 	(void)av;
 
-	exit_code = (isnumber(arg[1])) ? _atoi(arg[1]) : 100;
+	exit_code = (!arg[1]) ? 0 : 2;
+	exit_code = (isnumber(arg[1])) ? _atoi(arg[1]) : exit_code;
+	if (_atoi(arg[1]) < 0)
+		dprintf(STDERR_FILENO, "%s: 1: exit: Illegal number: %s\n", av, arg[1]);
 
-	free_array(arg);
-	free_array(environ);
+	if (!arg)
+	{
+		free_array(arg);
+		free_array(environ);
+	}
 	_exit(exit_code);
 	return (0);
 }
